c81c5cd8eacfbc7a5408a7d43da90650ad9295c1
Harshil Goyal <harshilgoyal17@gmail.com>
2025-01-19 16:26:41 +0530

    Modified Lexer.ml

--- lexer.ml
+++ lexer.ml
@@ -100,9 +100,33 @@ type token = AUTO
   | COMMENT
 
 (*Referenced from https://www.quut.com/c/ANSI-C-grammar-y-2011.html *)
+let t_O =  (rg '0' '7') (*Octal Number Regex*)
+let t_D = (rg '0' '9')(*Decimal Number Regex*)
+let t_NZ = (rg '1' '9')(*Non Zero regex*)
+let t_LC = (rg 'a' 'z')(*Lower case regex*)
+let t_UC = (rg 'A' 'Z')(*Upper case regex*)
+let t_L = alt [t_LC; t_UC; char '_'](*AlphabetUnderscore regex*)
+let t_A = alt [t_LC; t_UC; char '_'; t_D](*Alphabet Number Underscore regex*)
+let t_H = alt [rg 'a' 'f'; rg 'A' 'F';t_D](*Hexadecimal Number regex*)
+let t_HP = seq [char '0'; alt [char 'x';char 'X']](*Hexadecimal Prefix regex*)
+let t_E = seq [alt [char 'e'; char 'E'];opt (alt[char '-'; char '+']);rep1 t_D](*Exponential regex*)
+let t_P = seq [alt [char 'p'; char 'P'];opt (alt[char '-'; char '+']);rep1 t_D](*Exponential regex*)
 
-let t_O =  (rg '0' '7')
-let t_D = (rg '0' '9')
-let t_NZ = (rg '1' '9')
+let t_FS = alt [char 'f'; char 'F'; char 'l'; char 'L'](*Float Suffix regex*)
+let t_IS = alt [str "ul";  str "Ul";  str "uL";  str "UL";  str "lu";  str "Lu"; 
+                str "lU";  str "LU";  str "llu"; str "llU"; str "LLu"; str "LLU"; 
+                str "ulu"; str "Ulu"; str "uLu"; str "ULu"](*Integer Suffix regex*)
+let t_CP = alt[char 'u';char 'U';char 'L'](*Unicode Character Literals eg u'x' / u'z' etc*)
+let t_SP = alt[str "u8"; char 'u'; char 'U'; char 'L'](*encoded String literal eg u8"Hello" etc*)
+let t_ES = seq [char '\\'; alt [
+        alt [char '\''; char '"'; char '?'; char '\\'; char 'a'; char 'b'; char 'f'; char 'n'; char 'r'; char 't'; char 'v'];
+        repn t_O 1 (Some 3);
+        seq [char 'x'; rep1 t_H]
+    ]](*Escape Sequence regex*)
+let t_WS = space
+let re_int = compile(seq[start;alt[
+              seq[t_HP;(rep1 t_H);opt t_IS]
+]])
+(*Above token regexes are extracted from Aniruddha Deb's Repository: https://github.com/Aniruddha-Deb/ccaml/tree/main *)
 
 
